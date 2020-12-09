8fa130942cfdd5a577e39df4e1dc99937428fc0d
Khenus <khenus.tan@gmail.com>
2020-12-09 18:52:11 +0800

    V0.52 WIP Global Scope

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import './index.css';
 
 import CorePage from './CorePage/CorePage.js';
 import reportWebVitals from './reportWebVitals';
 
+const initialState = {
+  currentData: {},
+}
+
+function newDataRedux(state = initialState, action) {
+  switch(action.type) {
+    case 'UPDATE_DATA':
+    return {
+      currentData: action.newData,
+    };
+  default:
+    return state;
+  }
+}
+
+const store = createStore(newDataRedux);
+
 ReactDOM.render(
   <React.StrictMode>
-    <CorePage />
+    <Provider store={store}>
+      <CorePage />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
